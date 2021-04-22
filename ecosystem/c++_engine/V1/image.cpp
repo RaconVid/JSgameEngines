459abb98a98125b1e909448cfba7bad025b0b3f6
bigdog0044 <jambo.peach@outlook.com>
2021-04-22 20:31:27 +0100

    engine update V1

---
+++ ecosystem/c++_engine/V1/image.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+//main function of this code
+string file;
+string whatFile;
+string fileLocation;
+
+string readImages(string file, string whatFile, string fileLocation){
+    
+}
--- ecosystem/c++_engine/V1/main.cpp
+++ ecosystem/c++_engine/V1/main.cpp
@@ -1,4 +1,12 @@
-#include  <iostream>
+//libs needed for program to work
+#include <iostream>
 using namespace std;
 
-test
+//files needed to help operate this
+#include "image.cpp"
+
+// this file is the main control over this V1 version of the engine
+
+int main(){
+
+}
