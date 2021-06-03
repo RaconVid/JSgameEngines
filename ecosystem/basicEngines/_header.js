7c13736516ac3f631b488cf94e54ff93406ddcee
racon <cabcon25@gmail.com>
2021-06-03 21:32:52 +0100

    hyperbolic update

--- ecosystem/basicEngines/_header.js
+++ ecosystem/basicEngines/_header.js
@@ -1,8 +1,15 @@
-importJavascriptFromSrc(
-	//"basicEngines/extraFunctions1.js",
-	//"basicEngines/images.js",
-	"basicEngines/gameLoop.js",
-	"basicEngines/io.js",
-	"basicEngines/JSON++.js",
-	"basicEngines/math.js"
-);
+{
+	let a=()=>importJavascriptFromSrc(
+		//"basicEngines/extraFunctions1.js",
+		//"basicEngines/images.js",
+		"basicEngines/io.js",
+		"basicEngines/JSON++.js",
+		"basicEngines/math.js",
+		"basicEngines/gameLoop.js",
+	);
+	if(document.body)a();
+	else{
+		window.addEventListener("onload",a);
+		window.onload=a;
+	}
+}
