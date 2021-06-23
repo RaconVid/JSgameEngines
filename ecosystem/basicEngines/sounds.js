6b25c836c98cb8b0dacd825331e6a3b2d4a97b9c
racon <cabcon25@gmail.com>
2021-06-23 19:32:04 +0100

    fix Sounds

--- ecosystem/basicEngines/sounds.js
+++ ecosystem/basicEngines/sounds.js
@@ -1,38 +1,37 @@
 "use strict";
-
-let Sounds=(function() {
-    // The sound argument to each function should be an HTMLAudioElement
-    // object (e.g. returned by Audio).
-
-    function play(sound) {
-	if (sound.readyState == 6) {
-	    // The sound can be played through to the end.
-	    sound.play();
-	} else {
-	    myAudioElement.addEventListener("canplaythrough", event => {
-		myAudioElement.play();
-	    });
+let Sounds={
+	meow:"sounds/Meow.wav",
+};
+Sounds=(function() {
+	// The sound argument to each function should be an HTMLAudioElement
+	// object (e.g. returned by Audio).
+	function play(sound) {
+		if (sound.readyState == 6) {
+			// The sound can be played through to the end.
+			sound.play();
+		} else {
+			sound.addEventListener("canplaythrough", event => {
+				sound.play();
+			});
+		}
 	}
-    }
-
-    function pause(sound) {
-	sound.pause();
-    }
-
-    function rewind(sound) {
-	// Rewind to the beginning.
-	sound.load();
-    }
-    
-    function start(sound) {
-	rewind(sound);
-	play(sound);
-    }
-
-    function stop(sound) {
-	pause(sound);
-	rewind(sound);
-    }
-    
-    return { play, pause, rewind, start };
+	function pause(sound) {
+		sound.pause();
+	}
+	function rewind(sound) {
+		// Rewind to the beginning.
+		sound.load();
+	}
+	function start(sound) {
+		rewind(sound);
+		play(sound);
+	}
+	function stop(sound) {
+		pause(sound);
+		rewind(sound);
+	}
+	return { play, pause, rewind, start };
 })();
+{
+	Sounds.start(new Audio("sounds/Meow.wav"));
+}
